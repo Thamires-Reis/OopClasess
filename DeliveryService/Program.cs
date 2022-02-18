fe55ef56f1829870eacf6ff14d4cbb5bb287aacb
Thamires-Reis <90706550+Thamires-Reis@users.noreply.github.com>
2022-02-18 13:17:31 +0000

    Add files via upload

---
+++ DeliveryService/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DeliveryService
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string[] zipCode = { "IE01", "IE02", "IE03", "IE04","IE05","IE06","IE07", "IE08", "IE09", "IE10" };
+            int[] code = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("Welcome to Ireland delivery service");
+            Console.WriteLine();
+            Console.WriteLine("To check if we delivery to your address enter your zip code");
+            Console.WriteLine();
+            string zip = Console.ReadLine();
+
+            bool contains = false;
+            int codeAddress = 0;
+            for (int i = 0; i < zipCode.Length; i++)
+            {
+                if (zip == zipCode[i])
+                {
+                   contains = true;
+                   codeAddress = code[i];
+                    break;
+                }
+            }
+
+            if (contains == true)
+            {
+                
+                Console.WriteLine("We delivery to your adress {0}", zip);
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Unfortunately we don't delivery to your adress");
+            }
+          
+
+        }
+    }
+}
---
+++ HiddenWord/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HiddenWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Hangman game");
+            string[] secretwords = new string[10];
+            secretwords[0] = "blue";
+            secretwords[1] = "moon";
+            secretwords[2] = "umbrella";
+            secretwords[3] = "holiday";
+            secretwords[4] = "sun";
+            secretwords[5] = "pool";
+            secretwords[6] = "friday";
+            secretwords[7] = "apple";
+            secretwords[8] = "beach";
+            secretwords[9] = "book";
+            Random randGen = new Random();
+            var secretWords = randGen.Next(0, 9);
+            string hiddenWord = secretwords[secretWords];
+            char[] guess = new char[hiddenWord.Length];
+            Console.Write("Please enter a letter: ");
+
+            for (int p = 0; p < hiddenWord.Length; p++)
+                guess[p] = '*';
+
+            while (true)
+            {
+                char userGuess = char.Parse(Console.ReadLine());
+                for (int j = 0; j < hiddenWord.Length; j++)
+                {
+                    if (userGuess == hiddenWord[j])
+                        guess[j] = userGuess;
+                }
+                Console.WriteLine(guess);
+            }
+        }
+    }
+}
