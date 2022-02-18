3d7196301a4c6bd36b4a14a66f3754fb8ba160df
Thamires-Reis <90706550+Thamires-Reis@users.noreply.github.com>
2022-02-18 13:19:29 +0000

    Add files via upload

---
+++ Time/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Enter the ammount of hours:");
+                int hours = Convert.ToInt32(Console.ReadLine());
+                int minutes = hours * 60;
+                Console.WriteLine(minutes + " Minutes");
+            }
+        }
+    }
